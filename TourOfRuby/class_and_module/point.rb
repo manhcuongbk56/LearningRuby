038040d734b000d869a7c08a6b27ecb16e699b63
manhcuong <manhcuong@example.com>
2019-12-18 22:23:05 +0700

    Add class variable to Point class

--- TourOfRuby/class_and_module/point.rb
+++ TourOfRuby/class_and_module/point.rb
@@ -2,9 +2,19 @@ class Point
   include Enumerable
   include Comparable
   attr_reader :x, :y
+  @@n = 0
+  @@total_x = 0
+  @@total_y = 0
   def intialize(x, y)
     @x, @y = x, y
+    @@n += 1
+    @@total_x += x
+    @@total_y += y
   end
+
+  ORIGIN = Point.new(0, 0)
+  UNIT_X = Point.new(1, 0)
+  UNIT_Y = Point.new(0, 1)
 
   def +(other)
     Point.new(@x + other.x, @y + other.y)
@@ -65,6 +75,18 @@ class Point
     @x**2 + @y**2 <=> other.x**2 + other.y**2
   end
 
+  def self.sum(*points)
+    x = y = 0
+    points.each {|p| x += p.x; y += p.y}
+    Point.new(x, y)
+  end
+  class << self
+    def report
+      puts "Number of points: #{@@n}"
+      puts "Average X coordinate: #{@@total_x.to_f/@@n}"
+      puts "Average Y coordinate: #{@@total_y.to_f/@@n}"
+    end
+  end
 end
 
 # the new method of the class object creates a new instance object, and then it
